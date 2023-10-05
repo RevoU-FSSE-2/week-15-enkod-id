710c83387702da7e6d0d5cc2596ae7a0c28566df
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2023-10-05 07:21:41 +0700

    connect db railway

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import { db } from "./config/db.connection";
+// import routes from "./routes/main.route";
+
+
+const app = express();
+const port = process.env.PORT;
+
+db.connect(function (err) {
+  if (err) {
+    console.log(err)
+    throw err;
+  }
+  console.log("DB Connected!");
+});
+
+app.use(bodyParser.json());
+// app.use(routes)
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
