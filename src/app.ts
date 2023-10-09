030909f88bb7e68b37257e8e12fd4953a2854579
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2023-10-09 19:39:19 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -24,17 +24,10 @@ app.use(bodyParser.json());
 
 app.use(cors(corsClientx))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://clinetx-week15.netlify.app/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
 app.use(cors(corsClienty))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://clienty-week15.netlify.app/');
+  res.header('Access-Control-Allow-Origin', ['https://clienty-week15.netlify.app/','https://clinetx-week15.netlify.app/']);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
