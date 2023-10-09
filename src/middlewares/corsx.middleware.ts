87570b6704ddaccd74eb739a68b0f71fb2d5270f
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2023-10-09 14:23:46 +0700

    fix cors

--- src/middlewares/corsx.middleware.ts
+++ src/middlewares/corsx.middleware.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import cors from "cors";
 
 const corsClientx = {
-    origin: "https://week-15-fe.netlify.app/",
+    origin: "https://week-15-fe.netlify.app",
     methods: "GET,POST",
     allowedHeaders: "Content-Type"
 }
